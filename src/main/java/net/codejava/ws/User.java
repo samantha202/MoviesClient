e5c23a98dd3b76a91581fc7cc7dcba2b7d39e319
noussi <noussi@10.3.103.51>
2021-10-14 13:43:56 +0200

    User_Class_Implemented

--- src/main/java/net/codejava/ws/User.java
+++ src/main/java/net/codejava/ws/User.java
@@ -1,5 +1,50 @@
 package net.codejava.ws;
-
 public class User {
 
+	private String email;
+	private String passWord;
+	private String firstName;
+	private String lastName;
+	
+	public User() 
+	{
+		
+	}
+	public User(String email, String passWord, String firstName, String lastName) 
+	{
+	
+		this.email = email;
+		this.passWord = passWord;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public User(String firstName, String lastName) 
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassWord() {
+		return passWord;
+	}
+	public void setPassWord(String passWord) {
+		this.passWord = passWord;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
