576e0a6d6714e7d15cc1281d5da210b380d04aa3
noussi <noussi@10.3.103.51>
2021-10-14 13:17:00 +0200

    Class_Actor

--- src/main/java/net/codejava/ws/Actors.java
+++ src/main/java/net/codejava/ws/Actors.java
@@ -1,5 +1,33 @@
+/*in order to be able to transmit the objects from the front to the back without worries, 
+the different ojects that exist on the front should also exist in the back */
+
 package net.codejava.ws;
 
 public class Actors {
-
+	public String fisrtName;
+	public String lastName;
+	
+	
+	public Actors() {}
+	public Actors(String fisrtName, String lastName) {
+		super();
+		this.fisrtName = fisrtName;
+		this.lastName = lastName;
+	}
+	public Actors(String fisrtName) {
+	
+		this.fisrtName = fisrtName;
+	}
+	public String getFisrtName() {
+		return fisrtName;
+	}
+	public void setFisrtName(String fisrtName) {
+		this.fisrtName = fisrtName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 }
